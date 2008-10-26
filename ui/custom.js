db1cb61810c0822b7fb657889f53c585bb376af0
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2008-10-26 17:15:32 +0000

    html for h1 slides only
    git-svn-id: https://cle.sk/repos/pub/presentations@947 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

--- ui/custom.js
+++ ui/custom.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	// if the slide contains just <h1> put it's text in single cell table
 	$(".slide").each(function () {
 		if (($(this).children().size() == 1) && ($(this).children().filter('h1').size() == 1)) {
-			var text = $(this).children().filter('h1').text();
-			$(this).html('<h1 class="hidden">'+text+'</h1><table class="singleCellTable"><tr><td>'+text+'</td></tr></table>');
+			var h1 = $(this).children().filter('h1');
+			$(this).html('<h1 class="hidden">'+$(h1).text()+'</h1><table class="singleCellTable"><tr><td>'+$(h1).html()+'</td></tr></table>');
 		}
 	});
 	// replace div class sigleThink with single cell table
